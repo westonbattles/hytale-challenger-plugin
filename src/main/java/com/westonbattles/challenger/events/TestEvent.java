759c1912202eee3c2b541b855f95f3cd6e0c16d2
irondiamond595 <neodotliu@gmail.com>
2026-03-07 01:02:49 -0800

    templates for projectile (projectile somewhat buggy still)

--- src/main/java/com/westonbattles/challenger/events/TestEvent.java
+++ src/main/java/com/westonbattles/challenger/events/TestEvent.java
@@ -1,8 +1,11 @@
 package com.westonbattles.challenger.events;
 
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 public class TestEvent {
 
--- src/main/java/com/westonbattles/challenger/interactions/TemplateInteraction.java
+++ src/main/java/com/westonbattles/challenger/interactions/TemplateInteraction.java
@@ -2,17 +2,26 @@ package com.westonbattles.challenger.interactions;
 
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Holder;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.math.vector.Vector3f;
 import com.hypixel.hytale.protocol.InteractionState;
 import com.hypixel.hytale.protocol.InteractionType;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.InteractionContext;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.hypixel.hytale.server.core.modules.physics.util.PhysicsMath;
+import com.hypixel.hytale.server.core.modules.projectile.ProjectileModule;
+import com.hypixel.hytale.server.core.modules.projectile.config.ProjectileConfig;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
@@ -37,7 +46,6 @@ public class TemplateInteraction extends SimpleInstantInteraction {
         }
 
         World world = commandBuffer.getExternalData().getWorld();
-        Store<EntityStore> store = commandBuffer.getExternalData().getStore();
         Ref<EntityStore> ref = interactionContext.getEntity();
         Player player = commandBuffer.getComponent(ref, Player.getComponentType());
         if (player == null) {
@@ -53,7 +61,38 @@ public class TemplateInteraction extends SimpleInstantInteraction {
             return;
         }
 
-        player.sendMessage(Message.raw("You used the template item! (id: " + itemStack.getItemId() + ")"));
-        //custom behavior
+        player.sendMessage(Message.raw("You used the item with id: " + itemStack.getItemId() + "!"));
+
+        world.execute(() -> {
+
+            Store<EntityStore> store = world.getEntityStore().getStore();
+
+            // get projectile config from corresponding json
+            ProjectileConfig config = ProjectileConfig.getAssetMap().getAsset("template_projectile_config");
+            if (config == null) {
+                return;
+            }
+
+            // get player position and facing
+            TransformComponent transform = store.getComponent(ref, TransformComponent.getComponentType());
+            if (transform == null) {
+                return;
+            }
+            Vector3d position = transform.getPosition().clone();
+            position.y += 1.6; // offset to player pov
+            Vector3f rotation = transform.getRotation().clone();
+            Vector3d direction = new Vector3d();
+            PhysicsMath.vectorFromAngles(rotation.getYaw(), rotation.getPitch(), direction);
+
+            // spawn projectile
+            Ref<EntityStore> projectileRef = ProjectileModule.get().spawnProjectile(
+                    ref,
+                    commandBuffer,
+                    config,
+                    position,
+                    direction
+            );
+            LOGGER.atInfo().log("Spawned projectile: " + projectileRef);
+        });
     }
 }
