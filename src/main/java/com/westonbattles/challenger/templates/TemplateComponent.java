941dca78e4359e0df8f42d3e2611cc2dc0ebc6a1
westonbattles <weston@battles.org>
2026-03-07 02:56:42 -0800

    made a templates package and deleted test event listener thing

--- src/main/java/com/westonbattles/challenger/templates/TemplateComponent.java
+++ src/main/java/com/westonbattles/challenger/templates/TemplateComponent.java
@@ -1,4 +1,4 @@
-package com.westonbattles.challenger.components;
+package com.westonbattles.challenger.templates;
 
 import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
