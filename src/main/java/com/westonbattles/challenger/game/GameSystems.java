2ca248480de2e5dc17b34fc400b757d59bfe148c
westonbattles <weston@battles.org>
2026-03-08 19:49:29 -0700

    removed check game start system since we can handle it with joining/leaving and readying/undreadying

--- src/main/java/com/westonbattles/challenger/game/GameSystems.java
+++ src/main/java/com/westonbattles/challenger/game/GameSystems.java
@@ -6,42 +6,17 @@ import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.RefChangeSystem;
+import com.hypixel.hytale.component.system.tick.TickingSystem;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.westonbattles.challenger.ChallengerPlugin;
 import com.westonbattles.challenger.components.PlayerComponent;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class GameSystems {
 
-	public static class CheckGameStartSystem extends RefChangeSystem<EntityStore, PlayerComponent> {
 
-		@Nullable
-		@Override
-		public Query<EntityStore> getQuery() {
-			return ChallengerPlugin.get().getPlayerComponentType();
-		}
-
-		@Nonnull
-		@Override
-		public ComponentType<EntityStore, PlayerComponent> componentType() {
-			return ChallengerPlugin.get().getPlayerComponentType();
-		}
-
-		@Override
-		public void onComponentAdded(@Nonnull Ref<EntityStore> ref, @Nonnull PlayerComponent playerComponent, @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
-		}
-
-		@Override
-		public void onComponentSet(@Nonnull Ref<EntityStore> ref, @Nullable PlayerComponent playerComponent, @Nonnull PlayerComponent t1, @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
-		}
-
-		@Override
-		public void onComponentRemoved(@Nonnull Ref<EntityStore> ref, @Nonnull PlayerComponent playerComponent, @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
-		}
-
-
-	}
 
 }
