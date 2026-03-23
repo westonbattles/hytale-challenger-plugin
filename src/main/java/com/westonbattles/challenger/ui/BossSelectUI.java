c01450828a869ea79eba1f1f91f6dc0961b60ff7
westonbattles <weston@battles.org>
2026-03-22 23:52:32 -0700

    Ready button implemented in the settings ui

--- src/main/java/com/westonbattles/challenger/ui/BossSelectUI.java
+++ src/main/java/com/westonbattles/challenger/ui/BossSelectUI.java
@@ -26,6 +26,7 @@ import com.westonbattles.challenger.ChallengerPlugin;
 import com.hypixel.hytale.protocol.SoundCategory;
 import com.hypixel.hytale.server.core.asset.type.soundevent.config.SoundEvent;
 import com.hypixel.hytale.server.core.universe.world.SoundUtil;
+import com.westonbattles.challenger.components.PlayerComponent;
 import com.westonbattles.challenger.game.GameManager;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.bson.types.Code;
@@ -39,11 +40,13 @@ public class BossSelectUI extends InteractiveCustomUIPage<BossSelectUI.Data> {
 
     private static final String PLAYER_DROPDOWN_ID = "PlayerList";
     private static final String EXIT_BUTTON_ID = "CloseButton";
+    private static final String READY_BUTTON_ID = "ReadyButton";
+    private static final String UNREADY_BUTTON_ID = "UnreadyButton";
     private static final String RANDOM_PLAYER_SELECT_BUTTON_ID = "RandomBossSelectButton";
 
     private static final int OPEN_SOUND = SoundEvent.getAssetMap().getIndex("UIOpen");
     private static final int CLOSE_SOUND = SoundEvent.getAssetMap().getIndex("UIClose");
-    private static final int RANDOM_ROLL_SOUND = SoundEvent.getAssetMap().getIndex("RandomRoll");
+
 
     public BossSelectUI(@Nonnull PlayerRef playerRef, @Nonnull CustomPageLifetime lifetime) {
         super(playerRef, lifetime, Data.CODEC);
@@ -65,11 +68,23 @@ public class BossSelectUI extends InteractiveCustomUIPage<BossSelectUI.Data> {
 
         uiCommandBuilder.set("#PlayerList.Entries", players);
         uiCommandBuilder.set("#PlayerList.Value", gameManager.getBoss().getUuid().toString());
+        uiCommandBuilder.set("#HomePage.Visible", true);
+        uiCommandBuilder.set("#SettingsPage.Visible", false);
+
+        PlayerComponent playerComponent = store.getComponent(ref, ChallengerPlugin.get().getPlayerComponentType());
+        if (playerComponent == null) return;
+        boolean ready = playerComponent.isReady();
+
+        uiCommandBuilder.set("#ReadyButton.Visible", !ready);
+        uiCommandBuilder.set("#UnreadyButton.Visible", ready);
 
 
         uiEventBuilder.addEventBinding(CustomUIEventBindingType.ValueChanged, "#"+PLAYER_DROPDOWN_ID, EventData.of("@SelectedPlayer", "#"+PLAYER_DROPDOWN_ID+".Value"), false);
         uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#"+RANDOM_PLAYER_SELECT_BUTTON_ID, EventData.of("ClickedButton", RANDOM_PLAYER_SELECT_BUTTON_ID), false);
-        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#"+EXIT_BUTTON_ID, EventData.of("ClickedButton", EXIT_BUTTON_ID), false);
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#"+EXIT_BUTTON_ID, EventData.of("ClickedButton" , EXIT_BUTTON_ID), false);
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#"+READY_BUTTON_ID, EventData.of("ClickedButton", READY_BUTTON_ID), false);
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#"+UNREADY_BUTTON_ID, EventData.of("ClickedButton", UNREADY_BUTTON_ID), false);
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.SelectedTabChanged, "#MainTabs", EventData.of("@SelectedTab", "#MainTabs.SelectedTab"), false);
 
         // Play open sound
         SoundUtil.playSoundEvent2dToPlayer(this.playerRef, OPEN_SOUND, SoundCategory.UI);
@@ -111,27 +126,66 @@ public class BossSelectUI extends InteractiveCustomUIPage<BossSelectUI.Data> {
         Player player = Objects.requireNonNull(store.getComponent(ref, Player.getComponentType()));
 
         switch (data.clickedButton) {
+            case READY_BUTTON_ID -> {
+
+                PlayerComponent playerComponent = ChallengerPlugin.get().getGameManager().getPlayerComponent(playerRef);
+                if (playerComponent != null) {
+                    playerComponent.toggleReady();
+                    gameManager.getWorld().sendMessage(Message.raw(playerRef.getUsername() + " ready!"));
+                }
+
+                // Update the UI
+                UICommandBuilder commandBuilder = new UICommandBuilder();
+                commandBuilder.set("#"+READY_BUTTON_ID+".Visible", false);
+                commandBuilder.set("#"+UNREADY_BUTTON_ID+".Visible", true);
+                sendUpdate(commandBuilder, false);
+            }
+            case UNREADY_BUTTON_ID -> {
+
+                PlayerComponent playerComponent = ChallengerPlugin.get().getGameManager().getPlayerComponent(playerRef);
+                if (playerComponent != null) {
+                    playerComponent.toggleReady();
+                    gameManager.getWorld().sendMessage(Message.raw(playerRef.getUsername() + " unready!"));
+                }
+
+                // Update the UI
+                UICommandBuilder commandBuilder = new UICommandBuilder();
+                commandBuilder.set("#"+READY_BUTTON_ID+".Visible", true);
+                commandBuilder.set("#"+UNREADY_BUTTON_ID+".Visible", false);
+                sendUpdate(commandBuilder, false);
+            }
             case RANDOM_PLAYER_SELECT_BUTTON_ID -> {
-                SoundUtil.playSoundEvent2dToPlayer(this.playerRef, RANDOM_ROLL_SOUND, SoundCategory.UI);
                 gameManager.setBossIndex(-1);
                 updateSelection();
             }
             case EXIT_BUTTON_ID -> player.getPageManager().setPage(ref, store, Page.None);
         }
 
-        if (data.selectedPlayer != null) {
+        if (!data.selectedPlayer.isEmpty()) {
             gameManager.setBossIndex(UUID.fromString(data.selectedPlayer));
         }
-        //sendUpdate();
+
+        if (!data.selectedTab.isEmpty()) {
+            UICommandBuilder tabUpdate = new UICommandBuilder();
+            tabUpdate.set("#HomePage.Visible", data.selectedTab.equals("Home"));
+            tabUpdate.set("#SettingsPage.Visible", data.selectedTab.equals("Settings"));
+            switch (data.selectedTab) {
+                case "Home" -> tabUpdate.set("#PageTitle.Text", "Main Menu:");
+                case "Settings" -> tabUpdate.set("#PageTitle.Text", "Settings:");
+            }
+            sendUpdate(tabUpdate, false);
+        }
     }
 
     public static class Data {
         public static final BuilderCodec<Data> CODEC = BuilderCodec.builder(Data.class, Data::new)
                 .append(new KeyedCodec<>("ClickedButton", Codec.STRING), (data, s) -> data.clickedButton = s, data -> data.clickedButton).add()
                 .append(new KeyedCodec<>("@SelectedPlayer", Codec.STRING), (data, s) -> data.selectedPlayer = s, data -> data.selectedPlayer).add()
+                .append(new KeyedCodec<>("@SelectedTab", Codec.STRING), (data, s) -> data.selectedTab = s, data -> data.selectedTab).add()
                 .build();
 
         private String clickedButton = "";
         private String selectedPlayer = "";
+        private String selectedTab = "";
     }
 }
